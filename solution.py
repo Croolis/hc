107c69bcd60504485625f5c3e437bea025483347
Ilya Zheleznikov <bkmz2@yandex.ru>
2019-02-28 21:31:47 +0300

    add id to photo model

--- solution.py
+++ solution.py
@@ -1,13 +1,19 @@
 import sys
 
 class Photo:
-	def __init__(self, isVertical, tags):
-		self._isVertical = isVertical
+	def __init__(self, is_vertical, tags, id):
+		self._is_vertical = is_vertical
 		self._tags = set(tags)
+		self._id = id
+
 	def get_tags(self):
 		return self._tags
-	def get_isVertical(self):
-		return self._isVertical
+
+	def get_is_vertical(self):
+		return self._is_vertical
+
+	def get_id(self):
+		return self._id
 
 def read_file():
 	filename = sys.argv[1]
@@ -15,11 +21,13 @@ def read_file():
 	# считывание количества линий
 	f.readline()
 	photos = []
+	id = 0
 	for line in f:
 		splittedLine = line.split(' ')
 		isVertical = splittedLine[0] == 'V'
 		tags = splittedLine[2:]
 		tags[-1] = tags[-1].replace("\n", "")
-		photos.append(Photo(isVertical, tags))
+		photos.append(Photo(isVertical, tags, id))
+		id += 1
 
 	return photos
