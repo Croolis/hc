4f7c2e831fbbe9d67c9a10a3b736f223a97ad0dc
yellalena <yellalena@gmail.com>
2019-02-28 22:57:59 +0300

    make pairs

--- solution.py
+++ solution.py
@@ -15,6 +15,9 @@ class Photo:
 
     def get_id(self):
         return str(self._id)
+    
+    def __repr__(self):
+        return 'id: ' + str(self._id) + ', tags: ' + repr(self._tags)
 
 
 def read_file(filename=None):
@@ -34,3 +37,24 @@ def read_file(filename=None):
         id += 1
 
     return photos
+
+def make_pairs(photos):
+    vertical = list(filter(lambda x: x.get_is_vertical(), photos))
+    res = []
+    used = []
+    for pair in itertools.combinations(vertical, 2):
+        tags1 = pair[0].get_tags()
+        tags2 = pair[1].get_tags()
+        id1 = pair[0].get_id()
+        id2 = pair[1].get_id()
+        if id1 not in used and id2 not in used:
+            total_n_of_tags = len(tags1.union(tags2))
+            # типа, какая доля тегов мы бы хотели чтоб совпадала
+            cool_percent = total_n_of_tags * 0.4
+            mutual_n_of_tags = len(tags1.intersection(tags2))
+            if mutual_n_of_tags <= cool_percent and mutual_n_of_tags >= 1:
+                tags = tags1.union(tags2)
+                id = ' '.join([id1, id2])
+                res.append(Photo(False, tags, id))
+                used.extend([id1, id2])
+    return res
