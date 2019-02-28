367cb23185c3519decb3e7d50f3183751adb41e8
Aleksei Samburov <aesamburov@gmail.com>
2019-02-28 23:09:49 +0300

    Something something

--- slide_grouping.py
+++ slide_grouping.py
@@ -1,33 +1,53 @@
 from random import score, freeze_answer
 from solution import read_file
 
+from time import time
+
 def group(photos):
+	used = [0] * len(photos)
 	current_photo = photos[0]
+	used[0] = 1
+
 	final_slides = [current_photo]
 
-	unused_photos = set(photos) - {current_photo}
-	used_photos = {current_photo}
 	total_score = 0
 
-	while len(unused_photos) > 0:
-		used_photo = None
+	counter = 1
+	while counter < len(photos):
+		counter += 1
 		max_score = -1
 
-		for unused_photo in unused_photos:
+		if counter % 10 == 0:
+			print(time())
+			print(counter)
+
+		limited_tries = 0
+		for i in range(len(photos)):
+			if used[i] == 1:
+				continue
+			unused_photo = photos[i]
+
 			current_score = score(current_photo, unused_photo)
 			if current_score > max_score:
 				max_score = current_score
-				used_photo = unused_photo
+				used_photo_index = i
+			limited_tries += 1
+			# if limited_tries > 1:
+			# 	break
 
-		final_slides.append(used_photo)
+		if counter % 1000 == 0:
+			print(time())
+		final_slides.append(photos[used_photo_index])
+		used[used_photo_index] = 1
 		total_score += max_score
-		unused_photos.remove(used_photo)
-		used_photos = used_photos.union({used_photo})
-		current_photo = used_photo
+
+		current_photo = photos[used_photo_index]
+		if counter % 1000 == 0:
+			print(time())
 
 	print(total_score)
 	return final_slides
 
-photos = read_file('data/a_example.txt')
+photos = read_file('data/b_lovely_landscapes.txt')
 slides = group(photos)
 freeze_answer(slides, 'bibib2.txt')
