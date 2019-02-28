714ca0d7c6c0970bab5f3633b81e758b8c9d8e46
Ilya Zheleznikov <bkmz2@yandex.ru>
2019-02-28 21:22:35 +0300

    Add file reading

--- solution.py
+++ solution.py
@@ -8,3 +8,18 @@ class Photo:
 		return self._tags
 	def get_isVertical(self):
 		return self._isVertical
+
+def read_file():
+	filename = sys.argv[1]
+	f = open(filename, 'r')
+	# считывание количества линий
+	f.readline()
+	photos = []
+	for line in f:
+		splittedLine = line.split(' ')
+		isVertical = splittedLine[0] == 'V'
+		tags = splittedLine[2:]
+		tags[-1] = tags[-1].replace("\n", "")
+		photos.append(Photo(isVertical, tags))
+
+	return photos
