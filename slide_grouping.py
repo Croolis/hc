c2cad98a4fe0afd3d08f278129f7132d064ef13d
Aleksei Samburov <aesamburov@gmail.com>
2019-02-28 23:50:24 +0300

    Batching

--- slide_grouping.py
+++ slide_grouping.py
@@ -17,11 +17,9 @@ def group(photos):
 		counter += 1
 		max_score = -1
 
-		if counter % 10 == 0:
-			print(time())
+		if counter % 1000 == 0:
 			print(counter)
 
-		limited_tries = 0
 		for i in range(len(photos)):
 			if used[i] == 1:
 				continue
@@ -31,12 +29,7 @@ def group(photos):
 			if current_score > max_score:
 				max_score = current_score
 				used_photo_index = i
-			limited_tries += 1
-			# if limited_tries > 1:
-			# 	break
 
-		if counter % 1000 == 0:
-			print(time())
 		final_slides.append(photos[used_photo_index])
 		used[used_photo_index] = 1
 		total_score += max_score
@@ -48,6 +41,9 @@ def group(photos):
 	print(total_score)
 	return final_slides
 
+
 photos = read_file('data/b_lovely_landscapes.txt')
-slides = group(photos)
+slides = []
+for i in range(10):
+	slides += group(photos[8000 * i:8000 * (i + 1)])
 freeze_answer(slides, 'bibib2.txt')
